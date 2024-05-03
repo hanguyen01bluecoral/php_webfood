b19b7abd11ed1cd3777b4c01a5f1fe3be3b070fb
hanguyen01bluecoral <hanguyen.01@bluecoral.vn>
2024-05-03 16:53:21 +0700

    Create admin/food

--- webshop/admin/add-food.php
+++ webshop/admin/add-food.php
@@ -3,7 +3,13 @@
         <div class="wrapper">
             <h1>Add food</h1>
 
-            <hr> <hr>
+            <br> <br>
+            <?php
+                if(isset($_SESSION['upload'])) {
+                    echo $_SESSION['upload'];
+                    unset($_SESSION['upload']);
+                }
+            ?>
 
             <form action="" method="POST" enctype="multipart/form-data">
                 <table class="tbl-30">
@@ -40,12 +46,31 @@
                         <td>
                             <select name="category" id="">
                                 <?php
-                                    $sql = "SELECT * FROM tbl_category WHERE active='yes'"
+                                    $sql = "SELECT * FROM tbl_category WHERE active='yes'";
+                                    $res = mysqli_query($conn, $sql);
+                                    $count = mysqli_num_rows($res);
+
+                                    if($count>0) {
+                                        //we have category
+                                        while($row=mysqli_fetch_assoc($res)) {
+                                            //get the value details of categories
+                                            $id = $row['id'];
+                                            $title = $row['title'];
+                                            ?>
+                                                 <option value="<?php echo $id; ?>"><?php echo $title; ?></option>
+                                            <?php
+                                        }
+                                    } else {
+                                        //we do not have category
+                                        ?>
+                                             <option value="0">No category found</option>
+                                        <?php
+                                    }
                                 ?>
 
-                                <option value="1">Food</option>
-                                <option value="2">Snacks</option>
-                                <option value="3">Drink</option>
+                                
+
+                              
                             </select>
                         </td>
                     </tr>
@@ -74,6 +99,74 @@
 
                 </table>
             </form>
+
+            <?php
+                //check the buttons is clicked 
+                if(isset($_POST['submit'])) {
+                    // echo "clicked";
+                    //1. lay data tu form
+                    $title = $_POST['title'];
+                    $des = $_POST['des'];
+                    $price = $_POST['price'];
+                    $category = $_POST['category'];
+                    //check radio button xe dang o trang thai noa
+                    if(isset($_POST['featured'])) {
+                        $featured = $_POST['featured'];
+                    }else {
+                        $featured = "no";
+
+                    }
+
+                    if(isset($_POST['active'])) {
+                        $active = $_POST['active'];
+                    }else {
+                        $active = "no";
+                        
+                    }
+                    //2. cap nhat hinh
+                    if(isset($_FILES['image']['name'])) {
+                        //get the details of the selected image
+                        $image_name = $_FILES['image']['name'];
+                        if($image_name != "") {
+                            $ext = end(explode('.', $image_name));
+                            $image_name = "Food-name-".rand(0000,9999).".".$ext;
+                            $src = $_FILES['image']['tmp_name'];
+                            $dst = "../images/food/".$image_name;
+                            $upload = move_uploaded_file($src, $dst);
+                            if($upload==false) {
+                                $_SESSION['upload'] = "Cap nhat thanh cong";
+                                header('location:'.SITEURL.'admin/add-food.php');
+                                die();
+                            } else {
+
+                            }
+                        }
+                    }else {
+                        $image_name = "";
+                    }
+                    //3. truyen du lieu vao database
+                    $sql2 = "INSERT INTO tbl_food SET 
+                            title = '$title',
+                            des = '$des',
+                            price = $price,
+                            image_name = '$image_name',
+                            category_id = $category,
+                            featured = '$featured',
+                            active = '$active'
+                    ";
+                    //thuc thi
+                    $res2 = mysqli_query($conn, $sql2);
+                    if($res2 == true) {
+                        $_SESSION['add'] = "Them san pham thanh cong";
+                        header('location:'.SITEURL.'admin/manage-food.php');
+                    } else {
+                        $_SESSION['add'] = "Them san pham that bai";
+                        header('location:'.SITEURL.'admin/manage-food.php');
+                    }
+
+                    
+                }
+            ?>
         </div>
     </div>
 <?php include('particals/footer.php'); ?>
--- webshop/admin/manage-food.php
+++ webshop/admin/manage-food.php
@@ -10,52 +10,85 @@
             <br>
             <!--button-->
             <a href="<?php echo SITEURL; ?>admin/add-food.php" class="btn-primary">Add food</a>
-            <br>
+            <br> <br>
+            <?php
+                if(isset($_SESSION['add'])) {
+                    echo $_SESSION['add'];
+                    unset($_SESSION['add']);
+                }
+
+                if(isset($_SESSION['delete'])) {
+                    echo $_SESSION['delete'];
+                    unset($_SESSION['delete']);
+                }
+
+                if(isset($_SESSION['upload'])) {
+                    echo $_SESSION['upload'];
+                    unset($_SESSION['upload']);
+                }
+
+                if(isset($_SESSION['unauthorize'])) {
+                    echo $_SESSION['unauthorize'];
+                    unset($_SESSION['unauthorize']);
+                }
+            ?>
 
             <table class="tbl-full">
                 <tr>
                     <th>S.N</th>
-                    <th>full Name</th>
-                    <th>Username</th>
-                    <th>Action</th>
+                    <th>Title</th>
+                    <th>Price</th>
+                    <th>Image</th>
+                    <th>Featured</th>
+                    <th>Active</th>
+                    <th>Actions</th>
                 </tr>
-                <tr>
-                    <td>1.</td>
-                    <td>Van Nam</td>
-                    <td>vannam</td>
-                    <td>
-                        <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger"> Delete Admin</a>
-                       
-                    </td>
-                </tr>
-                <tr>
-                    <td>2.</td>
-                    <td>Van Nam</td>
-                    <td>vannam</td>
-                    <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger"> Delete Admin</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>3.</td>
-                    <td>Van Nam</td>
-                    <td>vannam</td>
-                    <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger"> Delete Admin</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>4.</td>
-                    <td>Van Nam</td>
-                    <td>vannam</td>
-                    <td>
-                    <a href="#" class="btn-secondary">Update Admin</a>
-                        <a href="#" class="btn-danger"> Delete Admin</a>
-                    </td>
-                </tr>
+                <?php
+                    //create sql query to get all the food 
+                    $sql = "SELECT * FROM tbl_food";
+                    $res = mysqli_query($conn,$sql);
+                    $count = mysqli_num_rows($res);
+                    $sn=1;
+                    if($count>0) {
+                        while($row=mysqli_fetch_assoc($res)) {
+                            $id = $row['id'];
+                            $title = $row['title'];
+                            $price = $row['price'];
+                            $image_name = $row['image_name'];
+                            $featured = $row['featured'];
+                            $active = $row['active'];
+                            ?>
+                                <tr>
+                                    <td><?php echo $sn++; ?></td>
+                                    <td><?php echo $title; ?></td>
+                                    <td><?php echo $price; ?></td>
+                                    <td>
+                                        <?php 
+                                            if($image_name=="") {
+                                                echo "That bai";
+                                            } else {
+                                                ?>
+                                                    <img src="<?php echo SITEURL; ?>images/food/<?php echo $image_name; ?>" width="90" height="40" alt="">
+                                                <?php
+                                            }
+                                        ?>
+                                    </td>
+                                    <td><?php echo $featured; ?></td>
+                                    <td><?php echo $active; ?></td>
+                                    <td>
+                                        <a href="<?php echo SITEURL; ?>/admin/update-food.php?id=<?php echo$id; ?>" class="btn-secondary">Update Admin</a>
+                                        <a href="<?php echo SITEURL; ?>/admin/delete-food.php?id=<?php echo$id; ?>&image_name=<?php echo $image_name; ?>" class="btn-danger"> Delete Admin</a>
+                                    
+                                    </td>
+                                </tr>
+                            <?php
+
+                        }
+                    }else {
+                        echo "<tr><td colspan='7'>That bai. </td></tr>";
+                    }
+                ?>
+                
             </table>
         </div>
         
